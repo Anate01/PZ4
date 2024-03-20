b969f85865ce7d9368f5548b716d7c310265eb56
Anatolii Koval <anate.s@ukr.net>
2024-03-20 20:27:16 +0200

    add pz4-3-1 folder, remove 4-3

---
+++ PZ4-3-1/PZ4-3-1/Program.cs
@@ -0,0 +1,29 @@
+﻿/////////////////////////////////////// 3.1
+namespace PZ4_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write text: ");
+
+            string str = Console.ReadLine();
+            char[] charArray = str.ToLower().ToCharArray();
+            int count = 0;
+
+            Console.Write("What symbol do you want to find? - ");
+            string str2 = Console.ReadLine();
+            char[] wanted = str2.ToCharArray();
+
+            if (wanted.Length > 1) Console.WriteLine("Incorrect input");
+
+            foreach (char i in charArray)
+            {
+                if (i == wanted[0]) count++;
+            }
+
+            Console.WriteLine("Text: ``" + str + "`` include: " + charArray.Length + " symbols.");
+            Console.WriteLine("Text include: " + count + " " + wanted[0] + " symbols.");
+        }
+    }
+}
