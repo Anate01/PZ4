7f4994cebbc5c161c1f87dd4a20c60f3b5b578fa
Anatolii Koval <anate.s@ukr.net>
2024-03-20 20:23:57 +0200

    add pz4-2 folder

---
+++ PZ4-2/PZ4-2/Program.cs
@@ -0,0 +1,40 @@
+﻿//////////////////////////////  2
+namespace PZ4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[50];
+            Random rand = new Random();
+            int count = 0;  // кількість парних чисел
+            int k = 0;  // лічильник парних чисел
+
+            for (int i = 0; i < 50; i++)
+            {
+                nums[i] = rand.Next(0, 100);
+                if (nums[i] % 2 == 0) count++;
+            }
+
+            Console.WriteLine("Arrays include " + count + " pair numbers.");
+            Console.Write("It's: ");
+
+            if (count == 0) k = 0; 
+            else k = 1; // якщо парних чисел не 0, то лічильник уже має мати значення 1 для коректної роботи програми
+
+            foreach (var w in nums)
+            {
+                if (w % 2 == 0 && k != count) // виводимо парне число з комою, якщо воно не останнє парне число
+                {
+                    Console.Write(w + ", ");
+                    k++;
+                }
+                else if (w % 2 == 0 && k == count) Console.Write(w + ". "); // виводимо парне число з крапкою, якщо воно останнє парне число
+
+            }
+            
+            Console.WriteLine("   ");
+
+        }
+    }
+}
