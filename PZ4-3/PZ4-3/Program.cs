af3164eb7286337e32f19648298aeca4b81dbb6b
Anatolii Koval <anate.s@ukr.net>
2024-03-20 20:24:58 +0200

    add pz4-3 folder

---
+++ PZ4-3/PZ4-3/Program.cs
@@ -0,0 +1,18 @@
+﻿////////////////////////////////  3
+namespace PZ4_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Write text: ");
+
+            string str = Console.ReadLine();
+            char[] charArray = str.ToCharArray();
+
+            Console.WriteLine("Text: ``" + str + "`` include: " + charArray.Length + " symbols.");
+
+        }
+    }
+}
